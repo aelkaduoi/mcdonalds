cd3e4d046f822489938a84e97a7b0f3c36b9cdb1
aelkaduoi <94105854+aelkaduoi@users.noreply.github.com>
2024-04-05 10:17:50 +0200

    Add files via upload

---
+++ src/Controller/BezoekerController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class BezoekerController extends AbstractController
+{
+    #[Route('/bezoeker', name: 'app_bezoeker')]
+    public function index(): Response
+    {
+        return $this->render('bezoeker/index.html.twig', [
+            'controller_name' => 'BezoekerController',
+            'user' => 'Amine',
+        ]);
+    }
+}
